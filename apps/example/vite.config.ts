393463eb06bfee3f0c1e32e8c718b358d6e850f5
Nenad Brcic <neno@neno.ch>
2023-04-10 18:45:25 +0200

    WIP enable module federation

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "exampleApp",
+      remotes: {
+        search: "http://localhost:30010/dist/assets/remoteEntry.js",
+        playlistApp: "http://localhost:30020/dist/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
--- apps/search/vite.config.ts
+++ apps/search/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "search",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+        "./SearchAppContent": "./src/SearchAppContent"
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
